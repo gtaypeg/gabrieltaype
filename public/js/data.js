ba6fa4c16eb55a442099a5381262b7544c3128f2
gtaypeg <gabrieltaype2@gmail.com>
2020-10-26 00:20:14 -0400

    optimized

--- public/js/data.js
+++ public/js/data.js
@@ -1,25 +1,25 @@
 
 let works = [
     {
-        img: "rojatv.jpg",
+        img: "rojatv-min.jpg",
         title: "RojaTV",
         href: "http://rojatv.online",
         tecs: 'HTML, CSS, JS, PHP',
     },
     {
-        img: "facebook-video.jpg",
+        img: "facebook-video-min.jpg",
         title: "Facebook Video Downloader",
         href: "http://player.rapidplayer.xyz/",
         tecs: 'Next',
     },
     {
-        img: "topauriculares.jpg",
+        img: "topauriculares-min.jpg",
         title: "Los mejores auriculares inalámbricos",
         href: "https://www.topauricularesinalambricos.com/",
         tecs: 'HTML, CSS, JS',
     },
     {
-        img: "mi-cuenta-online.jpg",
+        img: "mi-cuenta-online-min.jpg",
         title: "Mi cuenta online",
         href: "https://www.micuentaonline.com/",
         tecs: 'HTML, CSS, JS, PHP',
@@ -27,49 +27,49 @@ let works = [
 ];
 let personal = [
     {
-        img: "wikipedia-viewer.jpg",
+        img: "wikipedia-viewer-min.jpg",
         title: "Wikipedia Viewer",
         href: "https://codepen.io/gtaypeg/full/ZEbpQvj",
         tecs: 'HTML, CSS, JS',
     },
     {
-        img: "local-weather.jpg",
+        img: "local-weather-min.jpg",
         title: "Local Weather",
         href: "https://codepen.io/gtaypeg/full/QWjErYx",
         tecs: 'HTML, CSS, JS',
     },
     {
-        img: "image-search.jpg",
+        img: "image-search-min.jpg",
         title: "Image Search",
         href: "https://gtaypegimagesearchabstractionlayeg.glitch.me/",
         tecs: 'HTML, CSS, JS, Node',
     },
     {
-        img: "javascript-calculator.jpg",
+        img: "javascript-calculator-min.jpg",
         title: "Javascript Calculator",
         href: "https://codepen.io/gtaypeg/full/vYEVdVo",
         tecs: 'React',
     },
     {
-        img: "tic-tac-toe.jpg",
+        img: "tic-tac-toe-min.jpg",
         title: "Tic tac toe",
         href: "https://codepen.io/gtaypeg/full/mdeMGXB",
         tecs: 'React',
     },
     {
-        img: "simon-game.jpg",
+        img: "simon-game-min.jpg",
         title: "Simon Game",
         href: "https://codepen.io/gtaypeg/full/vYNRqLm",
         tecs: 'React',
     },
     {
-        img: "game-of-life.jpg",
+        img: "game-of-life-min.jpg",
         title: "Game of Life",
         href: "https://codepen.io/gtaypeg/full/gOaNBwy",
         tecs: 'React',
     },
     {
-        img: "quotes.jpg",
+        img: "quotes-min.jpg",
         title: "Random Quotes",
         href: "https://codepen.io/gtaypeg/full/povVOVW",
         tecs: 'React',
